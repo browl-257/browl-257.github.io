0a845e9fef9b05a8d4fd4dff1add8a5ba69e191e
browl-257 <98789463+browl-257@users.noreply.github.com>
2022-02-08 20:22:08 -0500

    commit

---
+++ concert/end.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    //alert("[whatever-little-message-you-want]");
+    $("#press").click(function(){
+        $(frame).show();
+        });
+        $("#press").click(function(){
+            $(meet).show();
+            });
+            $("#frame").hover(function(){
+                $(restart).show();
+                });
+                
+
+      
+})
+      
